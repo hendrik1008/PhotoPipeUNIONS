3b4301980b4abbe9517786c09f01ba80e0729257
Hendrik Hildebrandt <hendrik@astro.ruhr-uni-bochum.de>
2021-03-01 17:32:20 +0100

    Included some del commands to free up memory. Not sure this is working.

--- scripts/mosaic/add_WCS_cuts_to_sum_image.py
+++ scripts/mosaic/add_WCS_cuts_to_sum_image.py
@@ -37,22 +37,33 @@ p=np.ones(NAXIS1*NAXIS2,dtype=np.int16)*NAXIS1
 x=np.mod(n,p)
 y=np.divide(n,p)
 # delete n and p here
+del n
+del p
 sky = wcs_header.all_pix2world(x,y,0)
 # delete x and y here
+del x
+del y
 ra = sky[0]
 dec = sky[1]
 # delete sky here
+del sky
 deccut1 = np.greater(dec, declow)
 deccut2 = np.less_equal(dec, dechigh)
 # delete dec here
+del dec
 racut1 = np.greater(ra, ralow)
 racut2 = np.less_equal(ra, rahigh)
 # delete ra here
+del ra
 if ralow < rahigh:
     wcscut = racut1 * racut2 * deccut1 * deccut2
 else:
     wcscut = np.logical_or(racut1, racut2) * deccut1 * deccut2
 # delete racut1/2 and deccut1/2 here
+del racut1
+del racut2
+del deccut1
+del deccut2
 
 wcscut = np.reshape(wcscut, (NAXIS2, NAXIS1))
 
