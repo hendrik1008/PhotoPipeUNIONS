c47a20007684169fcca2e4d781db2f294f4f37a1
Angus H Wright <awright@astro.rub.de>
2022-09-16 16:52:10 +0200

    QC figure change

--- scripts/mask_gaap_failures.R
+++ scripts/mask_gaap_failures.R
@@ -115,10 +115,10 @@ if (length(ind)>0) {
     magicaxis::magimage(1:nrow(mask$imDat),1:ncol(mask$imDat),raw.mask,stretch='lin',hi=1,lo=0,main='Original Sum Image')
     #Plot the data distribution 
     magicaxis::magimage(seq(xrange[1],xrange[2],by=pixpergrid[1]),seq(yrange[1],yrange[2],by=pixpergrid[2]),all.hist$map,stretch='lin',hi=0.95,lo=0,main='Data Distribution')
-    points(cat$Xpos[ind],cat$Ypos[ind],pch='.',col=ifelse(cat[[paste0("FLAG_GAAP_",filter)]][ind]==0,hsv(0.5,a=0.1),hsv(0.05,a=0.1)))
+    points(cat$Xpos[ind],cat$Ypos[ind],pch='.',col=ifelse(cat[[paste0("FLAG_GAAP_",filter)]][ind]==0,hsv(0.5,a=0.2),hsv(0.05,a=0.2)))
     #Plot the new sum image 
     magicaxis::magimage(1:nrow(mask$imDat),1:ncol(mask$imDat),mask$imDat,stretch='lin',hi=1,lo=0,main='New Sum Image')
-    points(cat$Xpos[ind],cat$Ypos[ind],pch='.',col=ifelse(cat[[paste0("FLAG_GAAP_",filter)]][ind]==0,hsv(0.5,a=0.1),hsv(0.05,a=0.1)))
+    points(cat$Xpos[ind],cat$Ypos[ind],pch='.',col=ifelse(cat[[paste0("FLAG_GAAP_",filter)]][ind]==0,hsv(0.5,a=0.2),hsv(0.05,a=0.2)))
     #Close the plot device
     dev.off()
 
