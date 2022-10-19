b95cbad19e685e0aab0888a75b86f8e1421631a6
Hendrik Hildebrandt <hendrik@astro.rub.de>
2022-10-19 18:15:40 +0000

    Keep header intact.

--- scripts/erase_FITS_bit.py
+++ scripts/erase_FITS_bit.py
@@ -8,12 +8,15 @@ bit_value = int(sys.argv[3])
 
 hdu = fits.open(fn)
 data = hdu[0].data
+header = hdu[0].header
 
 bit_mask = 255-bit_value
 
 data_new = np.bitwise_and(data,bit_mask)
 
 hdu_new = fits.PrimaryHDU(data_new)
+hdu_new.header = header
+
 if os.path.exists(fo):
     os.remove(fo)
 hdu_new.writeto(fo)
