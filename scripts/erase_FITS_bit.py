40337fab678b43059d830f9c9b47318b4284cc91
Hendrik Hildebrandt <hendrik@astro.rub.de>
2022-10-19 11:23:35 +0000

    Added a script to manipulate mask bits.

---
+++ scripts/erase_FITS_bit.py
@@ -0,0 +1,17 @@
+import astropy.io.fits as fits
+import numpy as np
+import sys
+
+fn = sys.argv[1] # assuming an 8-bit FITS image with no extension here
+fo = sys.argv[2]
+bit_value = int(sys.argv[3])
+
+hdu = fits.open(fn)
+data = hdu[0].data
+
+bit_mask = 255-bit_value
+
+data_new = np.bitwise_and(data,bit_mask)
+
+hdu_new = fits.PrimaryHDU(data_new)
+hdu_new.writeto(fo)
