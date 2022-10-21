99fde62473cecdee5292d388e97993df23106137
Hendrik Hildebrandt <hendrik@astro.rub.de>
2022-10-21 13:54:30 +0000

    Erase intermediate data products.

--- scripts/construct_commands_KiDSLegacy.sh
+++ scripts/construct_commands_KiDSLegacy.sh
@@ -1020,6 +1020,7 @@ do
 	            ${mdfield}/${field_name}_${filter}_mask_AW_all.fits \
               ${mdfield}/${field_name}_${filter}_mask_AW.fits \
               8 \;\ 
+      echo -n ${mdfield}/${field_name}_${filter}_mask_AW_all.fits \;\
     done
   
     #if [ -f ${THELIDATAPATH}/@THELIFILTER@/coadd_@THELIVERSION@/${field_name}_@THELIFILTER@.@THELIVERSION@.swarp.cut.flag.fits.gz ] && \
@@ -1030,9 +1031,10 @@ do
     #fi
   
   ### create the combined flag file
-  echo python @RUNROOT@/@SCRIPTPATH@/make_KIDS_bitmask_DR5.py \
+  echo -n python @RUNROOT@/@SCRIPTPATH@/make_KIDS_bitmask_DR5.py \
     ${field_name} @THELIVERSION@ \"r_SDSS u_SDSS g_SDSS i_SDSS\" \
-    ${mdfield} ${THELIDATAPATH} ${mdfield} @POINTINGLIMITSFILE@ ${mask}
+    ${mdfield} ${THELIDATAPATH} ${mdfield} @POINTINGLIMITSFILE@ ${mask} \;\
+  echo rm ${mdfield}/${field_name}_{u,g,r,i}_mask_AW.fits
 fi
 done
 
