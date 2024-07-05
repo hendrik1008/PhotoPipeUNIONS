94e3b0f9d5902352a22185d1cb0383e46ac8b04f
Hendrik Hildebrandt <hendrikhildebrandt@example.com>
2024-07-05 05:47:07 +0000

    Brought up to date.

--- scripts/gaussianise_QC.sh
+++ scripts/gaussianise_QC.sh
@@ -17,9 +17,10 @@
 export PYTHONPATH=@RUNROOT@/INSTALL/anaconda2/photopipe_env/bin/python2:@RUNROOT@/INSTALL/anaconda2/photopipe_env/lib/
 export PYTHONPATH=${PYTHONPATH}:@RUNROOT@/INSTALL/anaconda2/bin/python2:@RUNROOT@/INSTALL/anaconda2/lib/
 export NUMERIX=numpy
-export PATH=@RUNROOT@/INSTALL/anaconda2/bin/:${PATH}
 export PATH=@RUNROOT@/INSTALL/anaconda2/photopipe_env/bin/:${PATH}
+export PATH=@RUNROOT@/INSTALL/anaconda2/bin/:${PATH}
 export PATH=@RUNROOT@/INSTALL/wcstools-3.9.6/bin/:${PATH}
+export PATH=@RUNROOT@/INSTALL/tex/bin/x86_64-linux/:${PATH}
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:@RUNROOT@/INSTALL/anaconda2/photopipe_env/lib/:@RUNROOT@/INSTALL/anaconda2/lib/
 set -e
 #}}}
@@ -30,11 +31,8 @@ wd=$1   # work directory
 tile=$2 # tile name
 band=$3 # band of the image, i.e. u, g, r, i, z
 
-#python @RUNROOT@/@SCRIPTPATH@/chimney_plot.py \
-#       $wd $tile $band
-
-#python @RUNROOT@/@SCRIPTPATH@/image_plot.py \
-#       $wd $tile $band
+python @RUNROOT@/@SCRIPTPATH@/chimney_plot.py \
+       $wd $tile $band
 
 album -b 10 1 1 $wd/${tile}_${band}.fits > $wd/${tile}_${band}.small.fits
 album -b 10 1 1 $wd/${tile}_${band}.weight.fits > $wd/${tile}_${band}.weight.small.fits
@@ -47,3 +45,22 @@ fits2bitmap --min_percent 1 --max_percent 95 \
 	    $wd/${tile}_${band}.cleaned.small.fits
 fits2bitmap --min_percent 1 --max_percent 99 \
 	    $wd/${tile}_${band}.weight.small.fits
+
+test -s $wd/${tile}_${band}_smart_ggpsf_map.ps && \
+    ps2pdf $wd/${tile}_${band}_smart_ggpsf_map.ps $wd/${tile}_${band}_smart_ggpsf_map.pdf
+test -s $wd/${tile}_${band}_smart_ker.map.ps && \
+    ps2pdf $wd/${tile}_${band}_smart_ker.map.ps $wd/${tile}_${band}_smart_ker.map.pdf
+test -s $wd/${tile}_${band}_smart_ggpsfstarpos.ps && \
+    ps2pdf $wd/${tile}_${band}_smart_ggpsfstarpos.ps $wd/${tile}_${band}_smart_ggpsfstarpos.pdf
+test -s $wd/${tile}_${band}_smart_ggpsfsticks.ps && \
+    ps2pdf $wd/${tile}_${band}_smart_ggpsfsticks.ps $wd/${tile}_${band}_smart_ggpsfsticks.pdf
+
+cp @RUNROOT@/@CONFIGPATH@/QC_landscape.tex $wd/${tile}_${band}_QC.tex
+
+for OPT in tile band
+do
+    sed -i "s#\@${OPT}\@#${!OPT}#g" $wd/${tile}_${band}_QC.tex
+done
+
+cd $wd
+pdflatex -interaction=batchmode ${tile}_${band}_QC.tex
