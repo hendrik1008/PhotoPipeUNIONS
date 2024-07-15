fd3c223daceeafd0a196050e2b5511ad559d483f
Hendrik Hildebrandt <hendrik@astro.rub.de>
2024-07-15 13:07:56 +0000

    Added script to handle extinction via python2 (astropy).

---
+++ scripts/add_extinction_python2.py
@@ -0,0 +1,42 @@
+#!/users/hendrik/anaconda2/bin/python
+
+from astropy.io import fits
+import numpy as np
+import sys
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+from astropy import wcs
+
+inimage = fits.open('/arc/home/hendrik/src/PhotoPipeUNIONS/dustmaps/sfd/SFD_dust_4096_ngp.fits') # axis flipped!
+imagedata = inimage[0].data
+
+w = WCS('/arc/home/hendrik/src/PhotoPipeUNIONS/dustmaps/sfd/SFD_dust_4096_ngp.fits')
+
+incat = sys.argv[1]
+outcat = sys.argv[2]
+
+hdu = fits.open(incat)
+
+data = hdu[1].data
+cols = data.columns
+
+RA = data['ALPHA_J2000']
+Dec = data['DELTA_J2000']
+
+coords = SkyCoord(RA, Dec, unit='deg')
+
+pos=wcs.utils.skycoord_to_pixel(coords, w)
+
+ebv = []
+
+for k in range(np.shape(pos)[1]):
+    ebv.append(imagedata[int(pos[1][k]),int(pos[0][k])])
+
+ebv_col = fits.Column(name='EXTINCTION', format='E', array=ebv)
+
+new_cols = fits.ColDefs((ebv_col,))
+
+hdu[1] = fits.BinTableHDU.from_columns(cols + new_cols)
+hdu[1].name = 'OBJECTS'
+
+hdu.writeto(outcat)
