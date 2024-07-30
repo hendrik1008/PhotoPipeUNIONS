adc3ecd9854dde51eaeca456bee0b79bc74a0ac4
Hendrik Hildebrandt <hendrik@astro.rub.de>
2024-07-30 05:29:30 +0000

    Fine tuned S/G separation.

--- scripts/gaussianise_chip.sh
+++ scripts/gaussianise_chip.sh
@@ -68,10 +68,10 @@ Decmax=`dmstodecimal $Decmax_dms | gawk '{print $1}'`
 ### New star catalogue based on the image itself ###
 # saturation levels
 case $band in
-    "u")  sat_level=`dfits_theli $measurement_image | fitsort_theli -d SATURATE|awk '{print $2*0.5}'`; ZP=30;;
-    "g")  sat_level=1000; ZP=27;;
+    "u")  sat_level=`dfits_theli $measurement_image | fitsort_theli -d SATURATE|awk '{print $2*0.3}'`; ZP=30;;
+    "g")  sat_level=300; ZP=27;;
     "r")  sat_level=`dfits_theli $measurement_image | fitsort_theli -d SATURATE|awk '{print $2*0.5}'`; ZP=30;;
-    "i")  sat_level=`dfits_theli $measurement_image | fitsort_theli -d SATURATE|awk '{print $2*100}'`; ZP=30;; # HACK: not sure about the saturation level
+    "i")  sat_level=`dfits_theli $measurement_image | fitsort_theli -d SATURATE|awk '{print $2*50}'`; ZP=30;; # HACK: not sure about the saturation level
     "z")  sat_level=1000; ZP=27;; # HACK: not sure about any of this
 esac
 
@@ -104,7 +104,15 @@ then
     fmax=`awk '{if ($10==0 && $5<'$rad1'+0.5) print $3}' $wd/${measurement_image_base}_cat.asc | sort -gr |head -1`
     rad2=`awk '{if ($3>'$fmax'/30. && $5<'$rad1'+0.5 && $10==0) print $5}' $wd/${measurement_image_base}_cat.asc|$gaap_dir/kk/mode | awk '{printf "%f\n",$1}'`
     #awk '{if ($3>'$fmax'/30. && $5<'$rad2'+0.3 && $5>'$rad2'-0.3 && $10==0) print $0}' $wd/${measurement_image_base}_cat.asc > $wd/${measurement_image_base}_star_cat_GAaP.asc
-    awk '{if ($3>'$fmax'/30. && $5<'$rad2'+0.3 && $5>'$rad2'/2.0 && $10==0) print $0}' $wd/${measurement_image_base}_cat.asc > $wd/${measurement_image_base}_star_cat_GAaP.asc
+    if [ $band = "r" ] || [ $band = "i" ]
+    then
+	awk '{if ($3>'$fmax'/30. && $5<'$rad2'+0.3 && $5>'$rad2'/2.0 && $10==0) print $0}' $wd/${measurement_image_base}_cat.asc > $wd/${measurement_image_base}_star_cat_GAaP.asc
+    elif [ $band = "z" ]
+    then
+	awk '{if ($3>'$fmax'/30. && $5<'$rad2'+0.7 && $5>'$rad2'/2.0 && $10==0) print $0}' $wd/${measurement_image_base}_cat.asc > $wd/${measurement_image_base}_star_cat_GAaP.asc
+    else
+	awk '{if ($3>'$fmax'/30. && $5<'$rad2'+1.0 && $5>'$rad2'/2.0 && $10==0) print $0}' $wd/${measurement_image_base}_cat.asc > $wd/${measurement_image_base}_star_cat_GAaP.asc
+    fi
     echo $rad1 $rad2 $fmax `wc -l $wd/${measurement_image_base}_star_cat_GAaP.asc`
    
    ### Start the GaAP processing
