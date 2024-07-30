b081bf6657214f3be32a565278db0ee637bdcff5
Hendrik Hildebrandt <hendrik@astro.rub.de>
2024-07-30 05:34:13 +0000

    Minor edit.

--- scripts/CANFAR_launch.sh
+++ scripts/CANFAR_launch.sh
@@ -5,15 +5,24 @@ tile_no_end=$2
 shift
 shift
 
-awk '{if (NR>='$tile_no_start' && NR<='$tile_no_end') print $0}' \
-    @RUNROOT@/r_tiles.txt \
-    > @RUNROOT@/r_tile_${tile_no_start}t${tile_no_end}.txt
-
 source @RUNROOT@/INSTALL/anaconda2/bin/activate
 
+awk '{if (NR>='$tile_no_start' && NR<='$tile_no_end') print $0}' \
+    @RUNROOT@/r_tiles_rest.txt \
+    > @RUNROOT@/r_tiles_rest_${tile_no_start}t${tile_no_end}.txt
 bash @RUNROOT@/run_PhotoPipe.sh \
-     @RUNROOT@/r_tile_${tile_no_start}t${tile_no_end}.txt \
+     @RUNROOT@/r_tiles_rest_${tile_no_start}t${tile_no_end}.txt \
      ${tile_no_start}t${tile_no_end} \
      CONVERT PREPARE GAUSSIANISE GAAP SDSSPREP ZPREP COMPTILE MERGE \
-     BPZ BPZ5 COMPTILEZ MASK QC CLEAN COPY \
-     >& @RUNROOT@/PhotoPipe_r_tile_${tile_no_start}t${tile_no_end}.log
+     BPZ COMPTILEZ MASK QC CLEAN COPY \
+     >& @RUNROOT@/PhotoPipe_r_tiles_rest_${tile_no_start}t${tile_no_end}.log
+
+#awk '{if (NR>='$tile_no_start' && NR<='$tile_no_end') print $0}' \
+#    @RUNROOT@/ugriz_tiles.txt \
+#    > @RUNROOT@/ugriz_tile_${tile_no_start}t${tile_no_end}.txt
+#bash @RUNROOT@/run_PhotoPipe.sh \
+#     @RUNROOT@/ugriz_tile_${tile_no_start}t${tile_no_end}.txt \
+#     ${tile_no_start}t${tile_no_end} \
+#     CONVERT PREPARE GAUSSIANISE GAAP SDSSPREP ZPREP COMPTILE MERGE \
+#     BPZ COMPTILEZ MASK QC CLEAN COPY \
+#     >& @RUNROOT@/PhotoPipe_ugriz_tile_${tile_no_start}t${tile_no_end}.log
