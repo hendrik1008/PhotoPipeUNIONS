479ff3840244f4fcfe8f699afed9f3615d1dd0ff
Hendrik Hildebrandt <hendrik@astro.rub.de>
2025-07-16 05:54:15 +0000

    Added z2 band.

--- scripts/QC/summarise_phot_comp.sh
+++ scripts/QC/summarise_phot_comp.sh
@@ -14,20 +14,20 @@ do
     for ending in "_" #_minaper1p0_ # _stars_ _stars0p7_ 
     do
 	echo $ending
-	#for filter in u g r i z 
-	#do
-	#    echo $filter
-	#    rm $wd/${filter}${ending}${survey}_offsets_tile.asc
-	#    for file in $md/UNIONS.*/phot_comp_$survey/UNIONS.???.???_ugriz_${survey}_${filter}_offset.asc
-	#    do
-	#	tile=`basename $file|cut -d "_" -f 1|cut -d "." -f 2-3`
-	#	WCS=`@RUNROOT@/INSTALL/anaconda2/bin/python @RUNROOT@/@SCRIPTPATH@/translate_THELI2MP.py $tile|sed 's/\_/\ /g'`
-	#	{ 
-	#	    echo -n $tile $WCS" "
-	#    	    awk '!/nan/' $file
-	#	} |awk 'NF==8' >> $wd/${filter}${ending}${survey}_offsets_tile.asc
-	#    done
-	#done
+	for filter in u g r i z z2
+	do
+	    echo $filter
+	    rm $wd/${filter}${ending}${survey}_offsets_tile.asc
+	    for file in $md/UNIONS.*/phot_comp_$survey/UNIONS.???.???_ugriz_${survey}_${filter}_offset.asc
+	    do
+		tile=`basename $file|cut -d "_" -f 1|cut -d "." -f 2-3`
+		WCS=`@RUNROOT@/INSTALL/anaconda2/bin/python @RUNROOT@/@SCRIPTPATH@/translate_THELI2MP.py $tile|sed 's/\_/\ /g'`
+		{ 
+		    echo -n $tile $WCS" "
+	    	    awk '!/nan/' $file
+		} |awk 'NF==8' >> $wd/${filter}${ending}${survey}_offsets_tile.asc
+	    done
+	done
 	
 	@RUNROOT@/INSTALL/anaconda2/bin/python @RUNROOT@/@SCRIPTPATH@/QC/summarise_phot_comp.py $wd ${ending} $survey \
 	       > $wd/phot_comp_summary${ending}_${survey}.txt
