31fa0906753ed1008cac0e2b9e435e26bc50ff6b
Hendrik Hildebrandt <hendrik@astro.rub.de>
2025-12-13 09:55:46 +0000

    Added script to check for failures and delete incomplete products.

---
+++ scripts/check_fail.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+md=$1
+
+tile=`basename $md`
+
+# Check PREPARE failures.
+for band in u g r i z z2
+do
+    if [ -s $md/$band/${tile}_${band}.fits ]
+    then
+	size=`ls -l $md/$band/${tile}_${band}.fits|awk '{print $5}'`
+	if [ $size -lt 400000000 ]
+	then
+	    rm -rf $md/$band
+	    rm -rf $md/${tile}*
+	    rm -rf $md/BPZ*
+	    rm -rf $md/phot_comp*
+	fi
+    fi
+done
+
