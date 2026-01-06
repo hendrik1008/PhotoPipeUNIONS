3001212d6d2ec6feb3e0c63cd8f2ab5b386ebcda
Hendrik Hildebrandt <hendrik@astro.rub.de>
2026-01-06 10:12:34 +0000

    Added griz case.

--- scripts/compare_z.sh
+++ scripts/compare_z.sh
@@ -78,6 +78,11 @@ then
     echo
     ldacfilter -i $wd/merg_spec_comp.cat_$$ -o $wd/merg_spec_comp2.cat_$$ \
 	       -t PSSC -c "((((RICHNESS>1)AND(MAG_GAAP_u>0))AND(MAG_GAAP_r>0))AND(MAG_GAAP_i>0))AND(MAG_GAAP_z>0);"
+elif [ $filters = "griz" ]
+then
+    echo
+    ldacfilter -i $wd/merg_spec_comp.cat_$$ -o $wd/merg_spec_comp2.cat_$$ \
+	       -t PSSC -c "((((RICHNESS>1)AND(MAG_GAAP_g>0))AND(MAG_GAAP_r>0))AND(MAG_GAAP_i>0))AND(MAG_GAAP_z>0);"
 elif [ $filters = "ugriz" ]
 then
     echo
