1474a1dd656ba4ceb432d4e765cef232a62df42a
Hendrik Hildebrandt <hendrik@astro.rub.de>
2026-05-14 07:43:50 +0000

    Added more failure modes.

--- scripts/check_fail.sh
+++ scripts/check_fail.sh
@@ -3,6 +3,26 @@
 md=$1
 
 tile=`basename $md`
+xxx=`echo $tile|cut -d "." -f 2`
+yyy=`echo $tile|cut -d "." -f 3`
+
+# Check CONVERT failures.
+size=`ls -l $md/CFIS.${xxx}.${yyy}.r.ldac.cat|awk '{print $5}'`
+size_SP=`ls -l $md/CFIS_SP.${xxx}.${yyy}.r.ldac.cat|awk '{print $5}'`
+if [ $size -lt 20000 ] || [ $size_SP -lt 60000 ]
+then
+    rm -rf $md/CFIS.${xxx}.${yyy}.r.ldac.cat
+    rm -rf $md/{u,g,r,i,z,z2}/${tile}_r_smart.gaap
+    rm -rf $md/{u,g,r,i,z,z2}/${tile}_r_smart_full*
+    rm -rf $md/{u,g,r,i,z,z2}/${tile}_r_smart_minaper*
+    rm -rf $md/CFIS_SP.${xxx}.${yyy}.r.ldac.cat
+    rm -rf $md/{u,g,r,i,z,z2}/${tile}_SP_r_smart.gaap
+    rm -rf $md/{u,g,r,i,z,z2}/${tile}_SP_r_smart_full*
+    rm -rf $md/{u,g,r,i,z,z2}/${tile}_SP_r_smart_minaper*
+    rm -rf $md/${tile}*
+    rm -rf $md/BPZ*
+    rm -rf $md/phot_comp*
+fi
 
 # Check PREPARE failures.
 for band in u g r i z z2
@@ -36,3 +56,13 @@ do
 	fi
     fi
 done
+
+# Check MASK failure.
+if [ -s $md/${tile}_ugriz.mask.fits ]
+then
+    size=`ls -l $md/${tile}_ugriz.mask.fits|awk '{print $5}'`
+    if [ $size -lt 100 ]
+    then
+	rm -rf $md/${tile}_ugriz.mask.fits*
+    fi
+fi
